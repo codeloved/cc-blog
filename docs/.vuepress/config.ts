c68b3df6e778341d27bc63e4d42be6ef46819c09
zhengcc <chengcheng.zheng@evchong.com>
2024-04-19 13:48:29 +0800

    feat: 增加配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,11 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
-})
+  lang: "zh-CN",
+  title: "hello blog",
+  description: "a best front blog",
+});
